855a2a810a557276680ffbddd1e80aaeab68b7d0
vrdnl <verdanielat@gmail.com>
2025-02-26 15:43:04 +0700

    ui category save collection

--- src/components/ModalSummary.jsx
+++ src/components/ModalSummary.jsx
@@ -2,6 +2,43 @@ import React from 'react';
 import { Modal, Box, Typography, Grid, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
+const tags = [
+  { label: 'Kulit Berminyak', emoji: '💧' },
+  { label: 'Komedo', emoji: '⚫' },
+  { label: 'Flek', emoji: '🟤' }
+];
+
+const TagButtons = () => {
+  return (
+    <Box display="flex" gap={2}>
+      {tags.map((tag, index) => (
+        <Box
+          key={index}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 1,
+            bgcolor: '#F384A7',
+            color: 'white',
+            px: 2,
+            py: 1,
+            borderRadius: '20px',
+            fontSize: 1,
+            paddingTop:0.5,
+            paddingBottom:0.5,
+            marginBottom:2,
+            marginTop:2
+          }}
+        >
+          <Typography>{tag.emoji}</Typography>
+          <Typography sx={{fontSize:10}}>{tag.label}</Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+
 const Summary = ({ open, onClose }) => {
   return (
     <Modal open={open} onClose={onClose}>
@@ -13,7 +50,8 @@ const Summary = ({ open, onClose }) => {
         width: '100%',
         maxWidth: 400,
         backgroundColor: 'white',
-        borderRadius: 3,
+        borderTopLeftRadius: 3,
+        borderTopLeftRadius:3,
         p: 3,
         boxShadow: 24,
       }}>
@@ -23,6 +61,7 @@ const Summary = ({ open, onClose }) => {
             <CloseIcon />
           </IconButton>
         </Box>
+        <TagButtons/>
         <Typography variant="body2" sx={{ mt: 1, mb: 2, fontSize: 14 }}>
             Berdasarkan hasil analisis wajah, kondisi kulit Anda menunjukkan kecenderungan lebih dominan terhadap masalah flek dan komedo. Persentase flek pada kulit Anda mencapai 60%, sementara komedo sebesar 40%. Hal ini menunjukkan bahwa perawatan utama sebaiknya difokuskan pada mengatasi flek dengan penggunaan produk pencerah, perlindungan dari sinar matahari, serta bahan aktif seperti vitamin C, niacinamide, atau alpha arbutin. Selain itu, perawatan untuk mengatasi komedo juga penting agar pori-pori tetap bersih dan tidak tersumbat, seperti rutin melakukan eksfoliasi ringan serta menggunakan produk berbahan aktif seperti salicylic acid atau clay mask untuk membersihkan pori-pori secara mendalam.
         </Typography>
